4ef8b2f9ac5fe636c6126792f504970d902a6b4d
Indukant Sharma <indukant.42020@stu.upes.ac.in>
2026-09-13 18:53:28 +0000

    Day01/q2.c

---
+++ q2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+int a,b;
+printf("Enter the number");
+scanf("%d,%d &a,&b");
+int c;
+c = a+b;
+printf("your answer is %d",c);
+int d;
+d = a-b;//a>b
+printf("your answer is %d",d);
+int f;
+f = a*b;
+printf("your answer is %d",f);
+int q;
+q = a/b;
+return 0;
+}
+
