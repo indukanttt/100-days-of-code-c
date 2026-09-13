c71c29d7e4517a2a32480bcbb9aeb0b72716155b
Indukant Sharma <indukant.42020@stu.upes.ac.in>
2026-09-13 19:38:20 +0000

    Day04/q5.c

--- Day03/q5.c
+++ Day03/q5.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int a, b, c;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    c = a;
-    a = b;
-    b = c;
-
-    printf("After swapping:\n");
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-
-    return 0;
+int Celsius=0;
+int Fahrenheit=(Celsius*9%5)+32;
+printf("value of Fahrenheit=%d", Fahrenheit);
+printf("value of %d", Celsius);
+return 0;
 }
+
