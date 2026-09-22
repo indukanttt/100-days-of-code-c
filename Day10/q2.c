b202851360114b551aa2926b8cd7c43e1dda3d42
Indukant Sharma <indukant.42020@stu.upes.ac.in>
2026-09-22 16:39:02 +0000

    Add q2.c to Day10

---
+++ Day10/q2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+int a,b,c;
+printf("Enter two number");
+scanf("%d %d",&a,&b);
+c = a+b;
+printf("your answer is: %d\n",c);
+int d;
+d = a-b;
+printf("your answer is: %d\n",d);
+int e;
+e = a*b;
+printf("your answer is: %d\n",e);
+int t;
+t = a/b;
+printf("your answer is: %d\n",t);
+return 0;
+}
